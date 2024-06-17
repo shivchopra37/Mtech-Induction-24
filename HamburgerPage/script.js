e1b7084c8d41c253bbc8e9ccbece03458e3fc17d
Sushant-4444 <arorasushant4444@gmail.com>
2024-06-17 18:58:05 +0530

    update

---
+++ HamburgerPage/script.js
@@ -0,0 +1,21 @@
+document.querySelector('.Getintouch').addEventListener('mouseover', function() {
+    this.src = 'GET IN TOUCH (1).png'; // Image when hovered
+});
+
+document.querySelector('.Getintouch').addEventListener('mouseout', function() {
+    this.src = 'GET IN TOUCH.png'; // Image when not hovered
+});
+document.querySelector('.importanInfo').addEventListener('mouseover', function() {
+    this.src = 'IMPORTANT INFO. (1).png'; // Image when hovered
+});
+
+document.querySelector('.importanInfo').addEventListener('mouseout', function() {
+    this.src = 'IMPORTANT INFO..png'; // Image when not hovered
+});
+document.querySelector('.contactHamburger').addEventListener('mouseover', function() {
+    this.src = 'CONTACT (1).png'; // Image when hovered
+});
+
+document.querySelector('.contactHamburger').addEventListener('mouseout', function() {
+    this.src = 'CONTACT.png'; // Image when not hovered
+});
