28ef8699dfcf195b43c32cff749cbd0711b56414
Sushant-4444 <arorasushant4444@gmail.com>
2024-07-05 22:07:03 +0530

    updated

--- HamburgerPage/script.js
+++ HamburgerPage/script.js
@@ -1,21 +1,21 @@
 document.querySelector('.Getintouch').addEventListener('mouseover', function() {
-    this.src = 'GET IN TOUCH (1).png'; // Image when hovered
+    this.src = 'Images/GET IN TOUCH (1).png'; // Image when hovered
 });
 
 document.querySelector('.Getintouch').addEventListener('mouseout', function() {
-    this.src = 'GET IN TOUCH.png'; // Image when not hovered
+    this.src = 'Images/GET IN TOUCH.png'; // Image when not hovered
 });
 document.querySelector('.importanInfo').addEventListener('mouseover', function() {
-    this.src = 'IMPORTANT INFO. (1).png'; // Image when hovered
+    this.src = 'Images/IMPORTANT INFO. (1).png'; // Image when hovered
 });
 
 document.querySelector('.importanInfo').addEventListener('mouseout', function() {
-    this.src = 'IMPORTANT INFO..png'; // Image when not hovered
+    this.src = 'Images/IMPORTANT INFO..png'; // Image when not hovered
 });
 document.querySelector('.contactHamburger').addEventListener('mouseover', function() {
-    this.src = 'CONTACT (1).png'; // Image when hovered
+    this.src = 'Images/CONTACT (1).png'; // Image when hovered
 });
 
 document.querySelector('.contactHamburger').addEventListener('mouseout', function() {
-    this.src = 'CONTACT.png'; // Image when not hovered
+    this.src = 'Images/CONTACT.png'; // Image when not hovered
 });
